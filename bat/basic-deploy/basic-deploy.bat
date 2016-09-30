65cfebd86a15acc9cc954197afbefe79e2bb880a
chakan <admin@serpens.hu>
2016-09-30 20:30:08 +0200

    Several fixes and few modifications - Details below Backup directory removal and creation was fixed Index.php related lines were removed - I will create a use case file for PHP sites in the future Uploading the maintenance mode files again after dowloading remote files and deleting them - Less chance to trigger issue #1 Extended comments

--- bat/basic-deploy/basic-deploy.bat
+++ bat/basic-deploy/basic-deploy.bat
@@ -13,16 +13,21 @@ set backup_dir=YOUR_BACKUP_DIRECTORY
 set remote_dir=YOUR_REMOTE_DIRECTORY
 set site_dir_name=YOUR_SITE_DIRECTORY_NAME
 
-REM -------------------------
+REM ---------------------------------------------------------------------------
 REM Dont edit below this line
 
 REM Delete local backup
-rmdir c:\Users\Imre\Documents\NetBeansProjects\backup /s /q
-mkdir c:\Users\Imre\Documents\NetBeansProjects\backup
+rmdir %backup_dir% /s /q
+REM We need to wait here a bit, because sometimes it creates the dir before deleting it yay 
+ping 127.0.0.1 -n 1
+mkdir %backup_dir%
 
-REM echo -------------------------
-REM echo Local backup deleted
+REM echo ---------------------------------------------------------------------------
+REM echo Local backup deleted, we are starting maintenance mode next
 REM pause
+
+REM Set starting dir just in case the connection fails
+cd %backup_dir%
 
 REM FTP connection
 echo user %user%> ftp.tmp
@@ -33,56 +38,54 @@ REM Begin maintenance
 echo cd %remote_dir%>>ftp.tmp
 echo lcd %backup_dir%>>ftp.tmp
 echo get index.html>> ftp.tmp
-echo get index.php>> ftp.tmp
 echo get .htaccess>> ftp.tmp
-echo delete index.html>> ftp.tmp
-echo delete index.php>> ftp.tmp
-echo delete .htaccess>> ftp.tmp
 echo lcd %maintenance_dir%>> ftp.tmp
+echo delete index.html>> ftp.tmp
 echo put index.html>> ftp.tmp
+echo delete .htaccess>> ftp.tmp
 echo put .htaccess>> ftp.tmp
 echo quit>> ftp.tmp
 ftp -n -s:ftp.tmp %server%
 del ftp.tmp
 
-REM echo -------------------------
-REM echo Site was set into maintenance mode
+REM echo ---------------------------------------------------------------------------
+REM echo Site was set into maintenance mode, possible index.html and .htaccess were downloaded
+REM echo We are downloading and deleting remote files next
 REM pause
 
 REM Download server files
 cd %backup_dir%
-rename index.php index_original.php
-rename index.html index_original.html
-rename .htaccess .htaccess_original
 ncftpget -u %user% -p %password% -R -DD %server% %backup_dir% %remote_dir%
 cd %site_dir_name%
 del index.html
-del index.php
 del .htaccess
 cd ..
-rename index_original.php index.php
-rename index_original.html index.html
-rename .htaccess_original .htaccess
-copy index.php %backup_dir%\%site_dir_name%
 copy index.html %backup_dir%\%site_dir_name%
 copy .htaccess %backup_dir%\%site_dir_name%
 del index.html
-del index.php
 del .htaccess
 
-REM echo -------------------------
-REM echo Backup download completed
+REM echo ---------------------------------------------------------------------------
+REM echo Backup download completed and all files on the server were deleted
+REM echo We are about to enable maintenance mode again
+
+REM Upload maintenance mode files again before we start the upload
+cd %maintenance_dir%
+ncftpput -u %user% -p %password% -R %server% %remote_dir% index.html
+ncftpput -u %user% -p %password% -R %server% %remote_dir% .htaccess
+
+REM echo ---------------------------------------------------------------------------
+REM echo Maintenance mode was enabled again, upload is next
 REM pause
 
 REM Upload local files
 cd %resource_dir%
-rename index.php index_original.php
 rename index.html index_original.html
 rename .htaccess .htaccess_original
 ncftpput -u %user% -p %password% -R %server% %remote_dir% .\*
 
-REM echo -------------------------
-REM echo Resource upload completed
+REM echo ---------------------------------------------------------------------------
+REM echo Resource upload completed, we are going live next
 REM pause
 
 REM FTP connection
@@ -93,14 +96,13 @@ echo bin>> ftp.tmp
 REM End maintenance and go live
 echo cd %remote_dir%>>ftp.tmp
 echo delete index.html>> ftp.tmp
-echo delete .htaccess>> ftp.tmp
-echo rename index_original.php index.php>> ftp.tmp
 echo rename index_original.html index.html>> ftp.tmp
+echo delete .htaccess>> ftp.tmp
 echo rename .htaccess_original .htaccess>> ftp.tmp
 echo quit>> ftp.tmp
 ftp -n -s:ftp.tmp %server%
 del ftp.tmp
 
-REM echo -------------------------
-REM echo Site is now live
+REM echo ---------------------------------------------------------------------------
+REM echo Site is now live! You can access the backup here: %backup_dir%
 REM pause
